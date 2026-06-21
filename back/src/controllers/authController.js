faf93f617d568e313ba84b713792529746563217
Yan Cota <yancota10@gmail.com>
2026-06-21 12:22:14 -0300

    feat: add novo item no retorno do login pra teste de ci/cd

--- back/src/controllers/authController.js
+++ back/src/controllers/authController.js
@@ -42,7 +42,8 @@ exports.login = async (req, res) => {
 
         res.status(200).json({
             message: "Login bem-sucedido!",
-            token: token
+            token: token,
+            cicd: "Deploy CI/CD verificado com sucesso no Render!"
         });
 
     } catch (error) {
